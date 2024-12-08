f4e5f873816998298dfdc8ac3ed285bd16e9831c
Akashmahanavar <akashmahanavar@gmail.com>
2024-12-08 23:40:34 +0530

    Added functionality for adding question in the form

--- components/Form.js
+++ components/Form.js
@@ -1,13 +1,49 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Navbar from "./Navbar";
 import Link from "next/link";
 import previewArrow from "@/public/preview-arrow.svg";
 import Image from "next/image";
 import QuestionType from "./QuestionType";
+import { formData, questionTypes } from "@/Data/constants";
 
 const Form = () => {
-  const [title, setTitle] = useState("Untitled Form");
+  const [data, setData] = useState(formData);
+  const [showDropdown, setShowDropdown] = useState(false);
+  const dropdownRef = useRef(null);
+  const handleTitleChange = (e) => {
+    const newTitle = e.target.value;
+    setData((prevData) => ({
+      ...prevData,
+      title: newTitle,
+    }));
+  };
+  const handleAddQuestion = (type) => {
+    const newQuestion = {
+      id: data.questions.length + 1,
+      questionText: "",
+      type: type || "shortAnswer",
+      options: [],
+      required: false,
+      answer: "",
+    };
+    setData((prevData) => ({
+      ...prevData,
+      questions: [...prevData.questions, newQuestion], // Append the new question
+    }));
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setShowDropdown(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
   return (
     <div>
       <Navbar />
@@ -20,10 +56,9 @@ const Form = () => {
         <div className="flex justify-between items-center px-10 h-14 border-b-[1px]">
           <input
             type="text"
-            onChange={(e) => {
-              setTitle(e.target.value);
-            }}
-            value={title}
+            placeholder="Untitled Form"
+            onChange={handleTitleChange}
+            value={data.title}
             required
             className="text-gray-500 font-bold outline-none w-full"
           />
@@ -40,14 +75,35 @@ const Form = () => {
 
         <div className="flex-grow overflow-y-auto p-4">
           <div className="flex justify-center">
-            <Link
-              href="/formlists"
-              className="text-black border border-solid border-black px-4 py-1 rounded-xl hover:scale-105"
-            >
-              Add Question
-            </Link>
+            <div className="relative inline-block" ref={dropdownRef}>
+              <button
+                className="border-2 border-gray-300 px-3 py-1 rounded-xl"
+                onClick={() => setShowDropdown((prev) => !prev)}
+              >
+                + Add Question
+              </button>
+              {showDropdown && (
+                <ul className="absolute mt-2 bg-white border border-gray-300 rounded shadow-md z-10">
+                  {questionTypes.map((data) => (
+                    <li
+                      key={data.type}
+                      className="px-[10px] py-2 cursor-pointer hover:bg-gray-200"
+                      onClick={() => handleAddQuestion(data.type)}
+                    >
+                      {data.text}
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </div>
           </div>
-          <QuestionType />
+          <div className="my-2 flex flex-col gap-2">
+            {data?.questions?.map((data) => (
+              <React.Fragment key={data.id}>
+                <QuestionType data={data} />
+              </React.Fragment>
+            ))}
+          </div>
         </div>
 
         <div className="flex justify-between px-10 h-14 items-center border-t-[1px]">
